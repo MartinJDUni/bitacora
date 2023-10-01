0096f9632adb9a433e092ccfc3be14f4d03a9db7
MartinJDUni <128074166+MartinJDUni@users.noreply.github.com>
2023-10-01 11:29:14 -0600

    test

---
+++ src/pages/Layout/layout.jsx
@@ -0,0 +1,11 @@
+import style from "@/styles/Layout.module.css"
+
+export default function ({children}) {
+    return (
+        <div className={style.conteiner}>
+            <main className={style.main}>
+                {children}
+            </main>
+        </div>
+    )
+}
